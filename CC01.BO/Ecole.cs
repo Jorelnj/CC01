641831f7a24b17f563768f184d1fa2d416b674e3
djojunior4@gmail.com <djojunior4@gmail.com>
2020-11-19 17:07:45 +0100

    voilà ton BO bro!

---
+++ CC01.BO/Ecole.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC01.BO
+{
+    public class Ecole
+    {
+        public string NomEcole { get; set; }
+        public string Filiere { get; set; }
+        public int AnneeAcademique { get; set; }
+
+        public Ecole()
+        {
+
+        }
+
+        public Ecole(string nomEcole, string filiere, int anneeAcademique)
+        {
+            NomEcole = nomEcole;
+            Filiere = filiere;
+            AnneeAcademique = anneeAcademique;
+        }
+    }
+
+
+    
+}
---
+++ CC01.BO/Etudiant.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC01.BO
+{
+    public class Etudiant : Ecole
+    {
+        public string NomEtudiant { get; set; } 
+        public string PrenomEtudiant { get; set; }
+        public DateTime DateNaissance { get; set; }
+        public string Lieu { get; set; }
+        public int Telephone { get; set; }
+        public string Email { get; set; }
+        public string Identifiant { get; set; }
+
+        public Etudiant()
+        {
+
+        }
+
+        public Etudiant(string nomEtudiant, string prenomEtudiant, DateTime dateNaissance, string lieu, 
+            int telephone, string email, string identifiant )
+        {
+            NomEtudiant = nomEtudiant;
+            PrenomEtudiant = prenomEtudiant;
+            DateNaissance = dateNaissance;
+            Lieu = lieu;
+            Telephone = telephone;
+            Email = email;
+            Identifiant = identifiant;
+        }
+    }
+}
